9b617a75e558944b44dca2b8a9c21d0774f5e45e
ryanphuah <ryan.phuah@qdx.co>
2025-07-15 15:52:38 +0800

    Update test_hsa.sh

--- test_hsa.sh
+++ test_hsa.sh
@@ -1,1 +1,2 @@
-python hsa.py -p 1h0s_protein_crystalwat_init_5ns.prmtop -t 1h0s_5ns_autoimaged.dcd -l ligand_lastframealigned.pdb -f 5000 -d 10 -o 1h0s -c paperwats.pdb
+cd test
+python ../hsa.py -p 1h0s_protein_crystalwat_init_5ns.prmtop -t 1h0s_5ns_autoimaged.dcd -l ligand_lastframealigned.pdb -f 5000 -d 10 -o 1h0s -c paperwats.pdb
