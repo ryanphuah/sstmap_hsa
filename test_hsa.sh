10c20f4227af7dcb28c9b54d03de03b8a4df3abb
ryanphuah <ryan.phuah@qdx.co>
2025-07-17 17:43:38 +0800

    Update test_hsa.sh

--- test_hsa.sh
+++ test_hsa.sh
@@ -1,2 +1,4 @@
 cd test
-python ../hsa.py -p 1h0s_protein_crystalwat_init_5ns.prmtop -t 1h0s_5ns_autoimaged.dcd -l ligand_lastframealigned.pdb -f 5000 -d 10 -o 1h0s -c paperwats.pdb
+python ../hsa.py -p 1h0s_protein_crystalwat_init_5ns.prmtop -t 1h0s_5ns_autoimaged.dcd -l 1h0s_ligand_lastframealigned.pdb -f 5000 -s 1 -d 10 -o 1h0s -c 1h0s_paperwats.pdb -w TIP3P
+python ../hsa.py -p 5std_protein_crystalwat_init_5ns.prmtop -t 5std_5ns_autoimaged.dcd -l 5std_ligand_lastframealigned.pdb -f 5000 -s 1 -d 10 -o 5std -c 5std_paperwats.pdb -w TIP3P
+python ../hsa.py -p 2qwj_protein_crystalwat_init_5ns.prmtop -t 2qwj_5ns_autoimaged.dcd -l 2qwj_ligand_lastframealigned.pdb -f 5000 -s 1 -d 10 -o 2qwj -c 2qwj_paperwats.pdb -w TIP3P
