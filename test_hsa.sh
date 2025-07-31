d48e946e98bd7a8d54372c4cb71ace1ab3f39119
ryanphuah <ryan.phuah@qdx.co>
2025-07-31 10:06:43 +0800

    Update test_hsa.sh
    Updated script for water placement example

--- test_hsa.sh
+++ test_hsa.sh
@@ -2,3 +2,4 @@ cd test
 python ../hsa.py -p 1h0s_protein_crystalwat_init_5ns.prmtop -t 1h0s_5ns_autoimaged.dcd -l 1h0s_ligand_lastframealigned.pdb -f 5000 -s 1 -d 10 -o 1h0s -c 1h0s_paperwats.pdb -w TIP3P
 python ../hsa.py -p 5std_protein_crystalwat_init_5ns.prmtop -t 5std_5ns_autoimaged.dcd -l 5std_ligand_lastframealigned.pdb -f 5000 -s 1 -d 10 -o 5std -c 5std_paperwats.pdb -w TIP3P
 python ../hsa.py -p 2qwj_protein_crystalwat_init_5ns.prmtop -t 2qwj_5ns_autoimaged.dcd -l 2qwj_ligand_lastframealigned.pdb -f 5000 -s 1 -d 10 -o 2qwj -c 2qwj_paperwats.pdb -w TIP3P
+python ../hsa.py -p 1f0s_protein_crystalwat_init_5ns.prmtop -t 1f0s_5ns_autoimaged.dcd -l 1f0s_ligand_lastframealigned.pdb -f 5000 -s 1 -d 5 -o 1f0s -w TIP3P
